ef11c880bc5c36c1ce2f7e3f3d5d8362fe2be725
arbiansayh10-pro <vivobook0110@gmail.com>
2026-09-01 10:13:49 +0700

    cobain enak

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 
-// Paksa folder storage & bootstrap cache ke direktori /tmp Vercel
+// 1. Paksa folder storage & cache ke /tmp Vercel
 $storagePath = '/tmp/storage';
 $directories = [
     $storagePath . '/app/public',
@@ -17,10 +17,16 @@ foreach ($directories as $dir) {
     }
 }
 
+// 2. Set environment variable penting secara eksplisit
+putenv("APP_STORAGE_PATH={$storagePath}");
+putenv("VIEW_COMPILED_PATH={$storagePath}/framework/views");
+
 $_ENV['APP_STORAGE_PATH'] = $storagePath;
+$_ENV['VIEW_COMPILED_PATH'] = $storagePath . '/framework/views';
 $_ENV['APP_SERVICES_CACHE'] = '/tmp/bootstrap/cache/services.php';
 $_ENV['APP_PACKAGES_CACHE'] = '/tmp/bootstrap/cache/packages.php';
 $_ENV['APP_CONFIG_CACHE'] = '/tmp/bootstrap/cache/config.php';
 $_ENV['APP_ROUTES_CACHE'] = '/tmp/bootstrap/cache/routes.php';
 
+// 3. Panggil entrypoint bawaan Laravel
 require __DIR__ . '/../public/index.php';
