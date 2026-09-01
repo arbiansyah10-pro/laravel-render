d30b4a3f9b6680cc6a662c5df8351dc163f46299
arbiansayh10-pro <vivobook0110@gmail.com>
2026-09-01 10:24:31 +0700

    cobain123

--- api/index.php
+++ api/index.php
@@ -1,4 +1,24 @@
 <?php
 
-// Forward request ka public/index.php bawaan Laravel
+// 1. Siapkan folder sementara di /tmp
+$storageDirs = [
+    '/tmp/storage/app/public',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+    '/tmp/bootstrap/cache'
+];
+
+foreach ($storageDirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
+// 2. Override env storage
+putenv('APP_STORAGE_PATH=/tmp/storage');
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+
+// 3. Panggil index.php utama
 require __DIR__ . '/../public/index.php';
